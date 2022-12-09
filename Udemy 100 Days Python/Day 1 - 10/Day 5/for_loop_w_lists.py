22c71f11ecdb1ad4fc78aaac84e91f723a683396
Raspaholic <81385239+Raspaholic@users.noreply.github.com>
2022-12-08 22:43:34 -0600

    DAY 5 partial commit

---
+++ Udemy 100 Days Python/Day 1 - 10/Day 5/for_loop_w_lists.py
@@ -0,0 +1,5 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:  # Continuously re-assigns item in list to variable fruit then prints it
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
